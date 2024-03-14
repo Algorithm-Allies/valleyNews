982ea02113e5b6c6b01647bb9094ae35711d6134
williamvelichko <williamvelichko2003@gmail.com>
2024-03-13 17:43:16 -0700

    verfication know works by comparing jwt tokens

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { register, login } = require("../controllers/userControllersPostgress");
+const { register, login, verifiy } = require("../controllers/userControllers");
 
 router.get("/", (req, res) => {});
 router.post("/login", login);
 router.post("/register", register);
+router.get("/verify", verifiy);
 router.get("/logout", (req, res) => {});
 
 module.exports = router;

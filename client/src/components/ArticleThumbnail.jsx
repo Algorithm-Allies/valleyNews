57f89322e604501756fe3161b4c572535b230529
AlissonRoss <alisson@alisson.me>
2024-03-15 18:00:38 -0700

    More styling work on Thumbnails

--- client/src/components/ArticleThumbnail.jsx
+++ client/src/components/ArticleThumbnail.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-
+import TestImage from '../assets/test-image.png';
 function ArticleThumbnail() {
   return (
-    <div className="w-full shadow-gray-700 shadow-md bg-brown-100 p-2 rounded-lg">
-      <div className=""></div>
-      ArticleThumbnail
+   
+    <div className="w-48 shadow-gray-700 shadow-md bg-brown-100 p-2 rounded-lg flex items-center justify-contents-center flex-col">
+      <img className="object-cover h-48 w-96" src={TestImage}></img>
+      <h1>Hello World</h1>
+      Lorem Ipsum is simply dummy text of the printing and  typesetting industry. Lorem Ipsum has been the industry's standard dummy  text ever since the 1500s 
     </div>
   );
 }
--- client/src/pages/NewsPage.jsx
+++ client/src/pages/NewsPage.jsx
@@ -1,10 +1,14 @@
-import React from "react";
 import ArticleThumbnail from "../components/ArticleThumbnail";
 //page for different news categories based on selected category i.e local, sports, crime, government, education, etc
 function NewsPage() {
   return (
-    <div>NewsPage
-      <ArticleThumbnail/>
+    <div>
+      NewsPage
+      <div className="unset-border-box w-full shadow-gray-700 shadow-md bg-brown-400 p-8 rounded-lg flex flex-row gap-x-8 items-center justify-contents-center ">
+        <ArticleThumbnail/>
+        <ArticleThumbnail/>
+        <ArticleThumbnail/>
+      </div>
     </div>
   );
 }

2f76d6bb31c8c51e018f044694ddff4abd9b71b8
mnicokar <nicokar.mobin@gmail.com>
2024-03-16 21:00:22 -0700

    added turlock Scraper

---
+++ server/scrapeData.js
@@ -0,0 +1,23 @@
+//// IMPORTS ////
+const { writeFile } = require("fs/promises");
+const path = require("path");
+// Getting Scraper functions.
+const { turlockJournalScraper } = require("./scrapers/turlockScraper");
+
+//// FUNCTIONS ////
+
+async function scrapeData(city = "all") {
+  console.log("\n");
+  let articles = [];
+  articles = await turlockJournalScraper();
+  console.log(`Scraped ${articles.length} articles from The Turlock Journal`);
+  await writeFile(
+    path.join(process.cwd(), "articles.json"),
+    JSON.stringify(articles)
+  );
+}
+
+// Updates Scraped Data object and will write to JSON file.
+scrapeData("turlock");
+
+module.exports = { scrapeData };

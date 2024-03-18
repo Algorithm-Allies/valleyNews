fff9e82f3f325f4340a1eafe803104f2e3f797f6
czcarrizales <czcarrizales@yahoo.com>
2024-03-18 16:29:58 -0700

    initial style of navbar using tailwind

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <ul class="flex *:text-gray-600 border-blue-300 justify-center pt-2">
+      <li class="py-2 px-4 border-r-2 border-gray-300">
+        <a href="#" className="hover:text-yellow-600">Home</a>
+      </li>
+      <li class="py-2 px-4 border-r-2 border-gray-300">
+        <a href="#" className=" hover:text-yellow-600">Local</a>
+      </li>
+      <li class="py-2 px-4 border-r-2 border-gray-300">
+        <a href="#" className="hover:text-yellow-600">Sports</a>
+      </li>
+      <li class="py-2 px-4 border-r-2 border-gray-300">
+        <a href="#" className="hover:text-yellow-600">Crime</a>
+      </li>
+      <li class="py-2 px-4 border-r-2 border-gray-300">
+        <a href="#" className="hover:text-yellow-600">Subscribe</a>
+      </li>
+      <li class="py-2 px-4 border-r-2 border-gray-300">
+        <a href="#" className="hover:text-yellow-600">Staff</a>
+      </li>
+      <li class="py-2 px-4">
+        <a href="#" className="text-yellow-600 font-medium hover:text-yellow-600">Login</a>
+      </li>
+    </ul>
+  )
 }
 
 export default NavBar;

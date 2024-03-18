0fc4a0e35e96eeddf9672de64f05b96827a2d9a9
Jonathan Oliver <jonoliver1997@gmail.com>
2024-03-18 12:04:59 -0700

    request error handling

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -42,15 +42,20 @@ export async function action({ request }) {
       "Content-Type": "application/json",
     },
   });
-  // If, the user was succesful in registering in, we redirect them to home page
+  // If, the user was successful in registering in, we redirect them to home page
   if (res.ok) {
     return redirect("/");
   }
-  // Otherwise, something went wrong on the server, we will return whatever message the server returns.  For example, email already in use.
-  formValidationErrorResponse({
-    data: { email },
-    message: "The server error message",
-  });
+  // If, the user was not successful in registering in, we display the error message
+  else {
+    const errorResponse = await res.json();
+    const errorMessage = errorResponse.message;
+
+    formValidationErrorResponse({
+      data: { email },
+      message: errorMessage,
+    });
+  }
 }
 
 function Register() {

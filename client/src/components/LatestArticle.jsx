0e2daf6a3780126c844a183ed0e35829134a52c3
AlissonRoss <alirosssalazar@gmail.com>
2024-03-21 15:38:21 -0700

    more responsiveness on news page

--- client/src/components/LatestArticle.jsx
+++ client/src/components/LatestArticle.jsx
@@ -2,7 +2,7 @@ function LatestArticle(itemData) {
     return (
         <div className="unset-border-box shadow-gray-700 shadow-md bg-brown-400 p-8 rounded-lg flex flex-row max-md:flex-col md:gap-x-8 gap-4 md:justify-center md:w-[80vw]">
         {itemData.map((thumbnail, i) => (
-          <div className="shadow-gray-700 shadow-md bg-brown-100 p-3 rounded-lg flex justify-between flex-row" key={i}>
+          <div className="shadow-gray-700 shadow-md bg-brown-100 p-3 rounded-lg flex justify-between max-md:flex-col flex-row" key={i}>
             <img className="object-cover h-48 w-96" component="img" src={[thumbnail.articleImg]}></img>
             <div className="flex flex-col px-[2em] justify-center">
                 <div className='text-lg py-1.5 font-bold'>{thumbnail.articleTitle}</div>

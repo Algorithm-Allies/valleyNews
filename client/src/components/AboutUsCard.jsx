322280d22fcad72bef30e6b18b809afa8b59dbc1
czcarrizales <czcarrizales@yahoo.com>
2024-03-22 10:35:44 -0700

    adding alt text to images

--- client/src/components/AboutUsCard.jsx
+++ client/src/components/AboutUsCard.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const AboutUsCard = ({name, role, image}) => {
+const AboutUsCard = ({name, role, image, imageAltText}) => {
     return (
         <div className='flex flex-col items-center bg-white p-4 rounded-lg text-center'>
-            <img src={image} alt="" className='rounded-full w-32 h-32 object-cover' />
+            <img src={image} alt={imageAltText} className='rounded-full w-32 h-32 object-cover' />
             <h2 className='text-xl text-orange-500 font-bold mt-2'>{name}</h2>
             <p className='font-bold'>{role}</p>
             <div className='flex w-full justify-evenly mt-10'>
--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -7,6 +7,7 @@ function AboutUs() {
       name: 'Cloud Strife',
       role: 'Frontend Lead',
       image: 'https://practicaltyping.com/wp-content/uploads/2023/05/cloud-strife-ff7remake.jpg',
+      imageAltText: 'cloud strife',
       githubLink: '',
       linkedInLink: '',
       websiteLink: ''
@@ -15,6 +16,7 @@ function AboutUs() {
       name: 'Aerith Gainsborough',
       role: 'Backend Lead',
       image: 'https://media.zenfs.com/en/bbc_us_articles_995/2c134d15668cbd52a684884512dc776f',
+      imageAltText: 'aerith gainsborough',
       githubLink: '',
       linkedInLink: '',
       websiteLink: ''
@@ -23,6 +25,7 @@ function AboutUs() {
       name: 'Barrett Wallace',
       role: 'Frontend Engineer',
       image: 'https://pbs.twimg.com/media/ExvZ1LlXMAAXgti.jpg',
+      imageAltText: 'barrett wallace',
       githubLink: '',
       linkedInLink: '',
       websiteLink: ''
@@ -31,6 +34,7 @@ function AboutUs() {
       name: 'Tifa Lockhart',
       role: 'Frontend Designer',
       image: 'https://www.gamespot.com/a/uploads/screen_kubrick/1581/15811374/3653723-final%20fantasy%20vii%20remake_20200410133520.jpg',
+      imageAltText: 'tifa lockhart',
       githubLink: '',
       linkedInLink: '',
       websiteLink: ''
@@ -39,6 +43,7 @@ function AboutUs() {
       name: 'Yuffie Kisaragi',
       role: 'QA Tester',
       image: 'https://cdn.vox-cdn.com/thumbor/nwO7aRQg8TefrPX1k5E-qSPK5uU=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22658754/ff7rintergrade_aprilscreenshot_01_EeZEkS9e0.jpg',
+      imageAltText: 'yuffie kisaragi',
       githubLink: '',
       linkedInLink: '',
       websiteLink: ''
@@ -47,6 +52,7 @@ function AboutUs() {
       name: 'Zack Fair',
       role: 'Journalist',
       image: 'https://editors.dexerto.com/wp-content/uploads/2023/10/24/Copy-of-dexerto-feature-images-with-correct-dimensions-2023-10-24T140246.633.jpg',
+      imageAltText: 'zack fair',
       githubLink: '',
       linkedInLink: '',
       websiteLink: ''
@@ -56,7 +62,7 @@ function AboutUs() {
     {
       aboutUsDummyData.map((data => {
         return (
-          <AboutUsCard name={data.name} role={data.role} image={data.image} />
+          <AboutUsCard name={data.name} role={data.role} image={data.image} imageAltText={data.imageAltText} />
         )
       }))
     }

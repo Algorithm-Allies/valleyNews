d1c62e3b014557eb954f9841f37ded445a2ff6de
czcarrizales <czcarrizales@yahoo.com>
2024-03-29 11:06:13 -0700

    temporarily remove Central Valley News text

--- client/src/components/Auth/AuthLayout.jsx
+++ client/src/components/Auth/AuthLayout.jsx
@@ -5,14 +5,14 @@ function AuthLayout() {
     <div className="h-screen bg-brown-100">
       <NavBar />
       <div className="flex flex-col items-center justify-center max-w-[80vw] w-full mx-auto pt-[15vh]">
-        <div className="space-y-2 text-center mb-4">
+        {/* <div className="space-y-2 text-center mb-4">
           <h1 className="text-3xl leading-6 font-bold text-custom-orange">
             Central Valley News
           </h1>
           <p className="text-sm text-brown-400">
             Get the latest news from around the Central Valley!
           </p>
-        </div>
+        </div> */}
         <Outlet />
       </div>
     </div>

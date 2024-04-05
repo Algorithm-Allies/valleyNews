2876cbc38d642f7ca657baf8ae1b180acf3b989c
czcarrizales <czcarrizales@yahoo.com>
2024-04-05 13:14:19 -0700

    temporary comment out central valley news

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -8,14 +8,14 @@ function PageNotFound() {
   return (
     <div className="h-screen bg-brown-100">
       <div className="h-full flex flex-col justify-center max-w-lg w-full mx-auto">
-        <div className="space-y-2 text-center mb-4">
+        {/* <div className="space-y-2 text-center mb-4">
             <h1 className="text-3xl leading-6 font-bold text-custom-orange">
               Central Valley News
             </h1>
             <p className="text-sm text-brown-400">
               Get the latest news from around the Central Valley!
             </p>
-        </div>
+        </div> */}
         <AuthForm>
           <div className="flex flex-col items-center gap-2">
           <h1 className="text-4xl text-brown-100 pb-6">404 Page not Found</h1>

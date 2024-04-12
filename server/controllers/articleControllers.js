164aca4ae98b55c6b45298563a529827f7468d91
Jonathan Oliver <jonoliver1997@gmail.com>
2024-04-11 21:38:16 -0700

    remove uneeded controllers

--- server/controllers/articleControllers.js
+++ server/controllers/articleControllers.js
@@ -1,17 +1,5 @@
-const { json } = require("express");
 const db = require("../config/database");
 const { insertArticle } = require("../services/articleService");
-
-// POST /api/articles
-async function createNewArticles(req, res) {
-  try {
-    await createArticles(articles);
-    res.status(201).json({ message: "Articles created successfully" });
-  } catch (error) {
-    console.error("Error creating articles:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-}
 
 // Create articles -- bulk insert into database
 async function createArticles(req, res) {

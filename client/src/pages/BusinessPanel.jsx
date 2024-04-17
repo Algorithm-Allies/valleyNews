ccfdca97a390946d644ece72d93fe193cbb7af77
AlissonRoss <alisson@alisson.me>
2024-04-16 22:00:39 -0700

    More styling business panel

--- client/src/pages/BusinessPanel.jsx
+++ client/src/pages/BusinessPanel.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import BusinessNavBar from "../components/BusinessNavBar";
+import Trash from '../assets/trash-fill.png';
+import Pencil from '../assets/pencil-square.png';
+import NewsPaper from '../assets/newspaper.png';
 function BusinessPanel() {
   return  (<div className="h-screen bg-brown-100">
   <div className="h-full flex flex-col pt-8 max-w-[70vw] w-full mx-auto">
@@ -11,15 +14,57 @@ function BusinessPanel() {
       </div>
 
       <hr className="rounded-md border-y-8 border-brown-400 mb-10 "/>
-      <form className="flex flex-col w-100 ">
-            
-          
-
-      </form>
+      <table class="table-auto border-collapse border border-[#FCFCFC] bg-[#FCFCFC]">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>TITLE</th>
+            <th>ENGAGEMENTS</th>
+            <th>ACTIONS</th>
+          </tr>
+        </thead>
+        <tbody>
+        {articleData.map((item, i) =>
+          <tr key={i} className="even:bg-[#F2F2F2] odd:bg-[#FCFCFC]">
+            <td className="border border-[#FCFCFC] ">{item.id}</td>
+            <td className="border border-[#FCFCFC] ">{item.title}</td>
+            <td className="border border-[#FCFCFC] ">{item.engagements}</td>
+            <td className="border border-[#FCFCFC] flex flex-row justify-evenly">
+              <button><img src={Trash}/></button>
+              <button><img src={Pencil}/></button>
+              <button><img src={NewsPaper}/></button>
+            </td>
+          </tr>
+        )}
+        </tbody>
+      </table>
     </div>
 
   </div>
 </div>)
 }
+const articleData =[
+  {
+    id: 1,
+    title: "Test Article",
+    engagements: 100
 
+  },
+  {
+    id: 2,
+    title: "Test Article",
+    engagements: 100
+  },
+  {
+    id: 3,
+    title: "Test Article",
+    engagements: 100
+
+  },
+  {
+    id: 4,
+    title: "Test Article",
+    engagements: 100
+  }
+]
 export default BusinessPanel;

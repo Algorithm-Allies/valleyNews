140b5b52a83f13243ee1680af44a8f51c612eeca
AlissonRoss <alisson@alisson.me>
2024-04-16 21:23:15 -0700

    Initial styling for business panel

--- client/src/components/BusinessNavBar.jsx
+++ client/src/components/BusinessNavBar.jsx
@@ -1,7 +1,16 @@
 
 
 function BusinessNavBar() {
-  return <div>BusinessNavBar</div>;
+  return (
+  <div className="flex justify-center">
+    <ul className="flex flex-row w-[50vw] justify-between pb-10 center">
+      <li><a href="#home">Home</a></li>
+      <li><a href="#news">News</a></li>
+      <li><a href="#contact">Contact</a></li>
+      <li><a href="#about">About</a></li>
+    </ul>
+  </div>
+  )
 }
 
 export default BusinessNavBar;
--- client/src/pages/BusinessPanel.jsx
+++ client/src/pages/BusinessPanel.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-
+import BusinessNavBar from "../components/BusinessNavBar";
 function BusinessPanel() {
-  return <div>BusinessPanel</div>;
+  return  (<div className="h-screen bg-brown-100">
+  <div className="h-full flex flex-col pt-8 max-w-[70vw] w-full mx-auto">
+    <BusinessNavBar/>
+    <div className="flex flex-col w-100">
+      <div className="flex flex-row justify-between pb-4">
+        <h1 className="text-4xl text-black-100 m-0">Articles</h1>
+        <button className="w-1/4 py-2 mr-8 rounded bg-orange-400 text-white m-0">New Article</button>
+      </div>
+
+      <hr className="rounded-md border-y-8 border-brown-400 mb-10 "/>
+      <form className="flex flex-col w-100 ">
+            
+          
+
+      </form>
+    </div>
+
+  </div>
+</div>)
 }
 
 export default BusinessPanel;

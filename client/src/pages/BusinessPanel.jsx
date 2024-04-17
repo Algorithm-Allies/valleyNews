7f4342e60b3664b4a3ed62131226c9d2527384a5
AlissonRoss <alirosssalazar@gmail.com>
2024-04-17 10:16:18 -0700

    Business Panel NavBar

--- client/src/pages/BusinessPanel.jsx
+++ client/src/pages/BusinessPanel.jsx
@@ -7,7 +7,7 @@ function BusinessPanel() {
   return  (<div className="h-screen bg-brown-100">
   <div className="h-full flex flex-col pt-8 max-w-[70vw] w-full mx-auto">
     <BusinessNavBar/>
-    <div className="flex flex-col w-100">
+    <div className="flex flex-col w-100 pt-[10vh]">
       <div className="flex flex-row justify-between pb-4">
         <h1 className="text-4xl text-black-100 m-0">Articles</h1>
         <button className="w-1/4 py-2 mr-8 rounded bg-orange-400 text-white m-0">New Article</button>

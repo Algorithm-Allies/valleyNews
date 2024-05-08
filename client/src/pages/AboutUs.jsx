bd5165c0516a895de3df83caa036e389ff879bc2
mnicokar <nicokar.mobin@gmail.com>
2024-05-08 15:19:02 -0700

    added urls for linkedin and github and some images

--- client/src/pages/AboutUs.jsx
+++ client/src/pages/AboutUs.jsx
@@ -1,72 +1,99 @@
 import React from "react";
 import AboutUsCard from "../components/AboutUsCard";
 
+//import MobinImg from "../assets/Mobin.jpg";
+import ManuelImg from "../assets/Manuel.jpg";
+//import AlanImg from "../assets/Alan.jpg";
+import AlissonImg from "../assets/Alisson.jpg";
+//import WilliamImg from "../assets/William.jpg";
+import JonathanImg from "../assets/Jonathan.jpg";
+//import ChazzImg from "../assets/Chazz.jpg";
+
 function AboutUs() {
   const aboutUsDummyData = [
     {
-      name: 'Cloud Strife',
-      role: 'Frontend Lead',
-      image: 'https://practicaltyping.com/wp-content/uploads/2023/05/cloud-strife-ff7remake.jpg',
-      imageAltText: 'cloud strife',
-      githubLink: '',
-      linkedInLink: '',
-      websiteLink: ''
+      name: "Mobin Nicokar",
+      role: "Team Lead",
+      image:
+        "https://practicaltyping.com/wp-content/uploads/2023/05/cloud-strife-ff7remake.jpg",
+      imageAltText: "Mobin",
+      githubLink: "https://github.com/mnicokar",
+      linkedInLink: "https://www.linkedin.com/in/mnicokar/",
+      websiteLink: "",
     },
     {
-      name: 'Aerith Gainsborough',
-      role: 'Backend Lead',
-      image: 'https://media.zenfs.com/en/bbc_us_articles_995/2c134d15668cbd52a684884512dc776f',
-      imageAltText: 'aerith gainsborough',
-      githubLink: '',
-      linkedInLink: '',
-      websiteLink: ''
+      name: "Manuel Manriquez",
+      role: "FullStack Developer",
+      image: ManuelImg,
+      imageAltText: "Manuel",
+      githubLink: "https://github.com/manriquezmanny",
+      linkedInLink: "https://www.linkedin.com/in/manuel-manriquez-b2b8b3235/",
+      websiteLink: "",
     },
     {
-      name: 'Barrett Wallace',
-      role: 'Frontend Engineer',
-      image: 'https://pbs.twimg.com/media/ExvZ1LlXMAAXgti.jpg',
-      imageAltText: 'barrett wallace',
-      githubLink: '',
-      linkedInLink: '',
-      websiteLink: ''
+      name: "Alan Ibarra",
+      role: "FullStack Developer",
+      image: "https://pbs.twimg.com/media/ExvZ1LlXMAAXgti.jpg",
+      imageAltText: "Alan",
+      githubLink: "https://github.com/Ibarra11",
+      linkedInLink: "https://www.linkedin.com/in/alanjibarra/",
+      websiteLink: "",
     },
     {
-      name: 'Tifa Lockhart',
-      role: 'Frontend Designer',
-      image: 'https://www.gamespot.com/a/uploads/screen_kubrick/1581/15811374/3653723-final%20fantasy%20vii%20remake_20200410133520.jpg',
-      imageAltText: 'tifa lockhart',
-      githubLink: '',
-      linkedInLink: '',
-      websiteLink: ''
+      name: "Alisson Ross",
+      role: "Frontend Developer",
+      image: AlissonImg,
+      imageAltText: "Alisson",
+      githubLink: "https://github.com/AlissonRoss",
+      linkedInLink: "",
+      websiteLink: "",
     },
     {
-      name: 'Yuffie Kisaragi',
-      role: 'QA Tester',
-      image: 'https://cdn.vox-cdn.com/thumbor/nwO7aRQg8TefrPX1k5E-qSPK5uU=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22658754/ff7rintergrade_aprilscreenshot_01_EeZEkS9e0.jpg',
-      imageAltText: 'yuffie kisaragi',
-      githubLink: '',
-      linkedInLink: '',
-      websiteLink: ''
+      name: "William Velichko",
+      role: "FullStack Developer",
+      image:
+        "https://cdn.vox-cdn.com/thumbor/nwO7aRQg8TefrPX1k5E-qSPK5uU=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22658754/ff7rintergrade_aprilscreenshot_01_EeZEkS9e0.jpg",
+      imageAltText: "William",
+      githubLink: "https://github.com/williamvelichko",
+      linkedInLink: "https://www.linkedin.com/in/williamvelichko/",
+      websiteLink: "",
     },
     {
-      name: 'Zack Fair',
-      role: 'Journalist',
-      image: 'https://editors.dexerto.com/wp-content/uploads/2023/10/24/Copy-of-dexerto-feature-images-with-correct-dimensions-2023-10-24T140246.633.jpg',
-      imageAltText: 'zack fair',
-      githubLink: '',
-      linkedInLink: '',
-      websiteLink: ''
+      name: "Jonathan Oliver",
+      role: "FullStack Developer",
+      image:
+        JonathanImg,
+      imageAltText: "Jonathan",
+      githubLink: "https://github.com/jonoliver1997",
+      linkedInLink: "https://www.linkedin.com/in/jonathan-oliver-webdev/",
+      websiteLink: "",
     },
-  ]
-  return <div className="grid grid-cols-3 gap-4">
     {
-      aboutUsDummyData.map((data => {
-        return (
-          <AboutUsCard name={data.name} role={data.role} image={data.image} imageAltText={data.imageAltText} />
-        )
-      }))
-    }
-  </div>;
+      name: "Chazz Carrizales",
+      role: "Frontend Developer",
+      image:
+        "https://editors.dexerto.com/wp-content/uploads/2023/10/24/Copy-of-dexerto-feature-images-with-correct-dimensions-2023-10-24T140246.633.jpg",
+      imageAltText: "Chazz",
+      githubLink: "https://github.com/czcarrizales",
+      linkedInLink: "https://www.linkedin.com/in/chazz-carrizales/",
+      websiteLink: "",
+    },
+  ];
+  return (
+    <div className="grid grid-cols-3 gap-4">
+      {aboutUsDummyData.map((data) => (
+        <AboutUsCard
+          key={data.name}
+          name={data.name}
+          role={data.role}
+          image={data.image}
+          imageAltText={data.imageAltText}
+          githubLink={data.githubLink}
+          linkedInLink={data.linkedInLink}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default AboutUs;
